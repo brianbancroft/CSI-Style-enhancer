60cffe7616870631932d1921b02a1cd3cad19755
Brian Bancroft <hello@brianbancroft.ca>
2019-11-03 18:44:06 -0800

    ➕  Add ga

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import ReactGA from "react-ga";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKCODE);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
